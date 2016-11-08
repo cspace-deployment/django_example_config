b3b19ec9e1f769cf57b0085c355374bf48fc00df
John Lowe <jblowe@berkeley.edu>
2016-11-08 11:34:20 -0800

    PAHMA-1299: eliminate uneeded functions for this webapp

--- pahma/apps/osteology/urls.py
+++ pahma/apps/osteology/urls.py
@@ -7,6 +7,5 @@ urlpatterns = patterns('',
                        url(r'^/?$', views.direct, name='direct'),
                        url(r'^skeleton/$', views.skeleton, name='skeleton'),
                        url(r'^search/$', views.search, name='search'),
-                       url(r'^search/(?P<fieldfile>[\w-]+)$', views.loadNewFields, name='loadNewFields'),
                        url(r'^results/$', views.retrieveResults, name='retrieveResults'),
                        )
