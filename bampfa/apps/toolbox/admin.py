5c546c74ecb4f6e31b3c8ef09ca41809b0eff5a4
John Lowe <jblowe@berkeley.edu>
2017-02-27 22:54:49 -0800

    NOJIRA: remove new-fangled Toolbox from bampfa

--- bampfa/apps/toolbox/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AdditionalInfo
-
-class AdditionalInfoAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
-    list_display = ('name', 'slug', 'order', 'contentType')
-
-admin.site.register(AdditionalInfo, AdditionalInfoAdmin)
