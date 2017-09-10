cbe67797fdfe69118c446ef78da3604ef8c59677
John Lowe <jblowe@berkeley.edu>
2017-09-10 13:27:53 -0700

    DJAN-94: grouper is now a contributed app

--- ucjeps/apps/grouper/urls.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'jblowe'
-
-from django.conf.urls import patterns, url
-from grouper import views
-
-urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
-                       )
