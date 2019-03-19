4ce2c7c1a5767da921296bbb2dab90ffbbc60bdc
John Lowe <jblowe@berkeley.edu>
2019-03-19 10:59:22 -0700

    CC-360: script used by reset.sh to set env vars

---
+++ ucjeps/apps/csvimport/test/set-config-ucjeps-dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+export CSPACEURL="https://ucjeps-dev.cspace.berkeley.edu"
+export CSPACEUSER="xxx@berkeley.edu:xxxx"
+echo
+echo ">>>>> Environment variables set:"
+echo CSPACEURL  $CSPACEURL
+echo CSPACEUSER $CSPACEUSER
+echo
