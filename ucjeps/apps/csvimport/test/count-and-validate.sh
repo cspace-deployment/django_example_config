814cd91446930597346e84128d2b646f4084b67e
John Lowe <jblowe@berkeley.edu>
2019-03-21 12:34:46 -0700

    CC-361: additional test files and a new test script

---
+++ ucjeps/apps/csvimport/test/count-and-validate.sh
@@ -0,0 +1,7 @@
+PYTHON=python2.7
+set -x
+rm -f runlog.out
+nohup time $PYTHON DWC2CSpace.py $1.csv ../config/csvimport.cfg ../config/DWC2CSpace-v2.csv ../config/collectionobject-v2.xml $1.valid.csv /dev/null /dev/null count > runlog.out 2>&1
+grep -v Insec runlog.out > $1.count.log; rm runlog.out
+nohup time $PYTHON DWC2CSpace.py $1.csv ../config/csvimport.cfg ../config/DWC2CSpace-v2.csv ../config/collectionobject-v2.xml $1.valid.csv $1.invalid.csv $1.terms.csv validate > runlog.out 2>&1
+grep -v Insec runlog.out > $1.validate.log; rm runlog.out
