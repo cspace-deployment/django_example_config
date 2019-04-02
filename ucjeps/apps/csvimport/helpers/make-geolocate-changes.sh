0aa732626f9885aca88a602056a0fac902f42ba9
John Lowe <jblowe@berkeley.edu>
2019-04-02 11:26:55 -0700

    CC-444: better version of this helper script

--- ucjeps/apps/csvimport/helpers/make-geolocate-changes.sh
+++ ucjeps/apps/csvimport/helpers/make-geolocate-changes.sh
@@ -1,4 +1,6 @@
-perl -i -pe 's/georef batch tool ....\-..\-..; GeoLocate/GEOLocate/' howell.csv 
-perl -i -pe 's/georef batch tool ....\-..\-..\t/GEOLocate\t/' howell.csv 
-perl -i -pe 's/GeoLocate; GeoLocate\t/GEOLocate\t/' howell.csv 
-perl -i -pe 's/GeoLocate\t/GEOLocate\t/' howell.csv
+FILE=$1
+perl -i -pe 's/georef batch tool ....\-..\-..; GeoLocate/GEOLocate/' $FILE
+perl -i -pe 's/georef batch tool ....\-..\-..\t/GEOLocate\t/' $FILE
+perl -i -pe 's/GeoLocate; GeoLocate\t/GEOLocate\t/' $FILE
+perl -i -pe 's/GeoLocate\t/GEOLocate\t/' $FILE
+perl -i -pe 's/\tte\t/\tGEOLocate\t/' $FILE
