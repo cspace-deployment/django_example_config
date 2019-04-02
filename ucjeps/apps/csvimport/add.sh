1b63332f94562a2e27c9fd7c75ccb56a5c8ab7f1
John Lowe <jblowe@berkeley.edu>
2019-04-02 10:56:34 -0700

    CC-444: revised csvImport scripts to support add/update of authority records

--- ucjeps/apps/csvimport/add.sh
+++ ucjeps/apps/csvimport/add.sh
@@ -5,6 +5,6 @@ set -x
 rm -f runlog.out
 grep -v Insec runlog.out > $1.validate.log; rm runlog.out
 touch $1.inprogress.log
-nohup time $PYTHON DWC2CSpace.py $1.add.csv ../config/csvimport.cfg ../config/DWC2CSpace-v2.csv ../config/collectionobject-v2.xml $1.add.csv /dev/null /dev/null add > runlog.out 2>&1
+nohup time $PYTHON DWC2CSpace.py $1.add.csv ../config/csvimport.cfg ../config/DWC2CSpace-v2.csv ../config/collectionobject-v2.xml $1.add-audit.csv /dev/null /dev/null add $2 > runlog.out 2>&1
 grep -v Insec runlog.out > $1.added.log; rm runlog.out
 rm $1.inprogress.log
--- ucjeps/apps/csvimport/update.sh
+++ ucjeps/apps/csvimport/update.sh
@@ -4,7 +4,7 @@ PYTHON=python2.7
 set -x
 touch $1.inprogress.log
 rm -f runlog.out
-nohup time $PYTHON DWC2CSpace.py $1.update.csv ../config/csvimport.cfg ../config/DWC2CSpace-v2.csv ../config/collectionobject-v2.xml $1.update.csv /dev/null /dev/null update > runlog.out 2>&1
+nohup time $PYTHON DWC2CSpace.py $1.update.csv ../config/csvimport.cfg ../config/DWC2CSpace-v2.csv ../config/collectionobject-v2.xml $1.update.csv /dev/null /dev/null update $2 > runlog.out 2>&1
 grep -v Insec runlog.out > $1.updated.log; rm runlog.out
 rm $1.inprogress.log
 
