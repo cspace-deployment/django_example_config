3b76516403d454858b2e48b3469a615cd8569820
John Lowe <jblowe@berkeley.edu>
2019-04-10 10:12:20 -0700

    CC-444: revised csvImport shell scripts so that several task can run simultaneously

--- ucjeps/apps/csvimport/add-authority.sh
+++ ucjeps/apps/csvimport/add-authority.sh
@@ -2,10 +2,10 @@
 cd $( dirname "${BASH_SOURCE[0]}" )
 PYTHON=python2.7
 set -x
-rm -f runlog.out
-grep -v Insec runlog.out > $1.validate.log; rm runlog.out
+rm -f $1.runlog.out
+grep -v Insec $1.runlog.out > $1.validate.log; rm $1.runlog.out
 touch $1.inprogress.log
-time $PYTHON DWC2CSpace.py $1.input.csv ../config/csvimport.cfg ../config/fields.collectors.csv ../config/template.collectors.xml $1.add-audit.csv /dev/null /dev/null add $2 > runlog.out 2>&1
-grep -v Insec runlog.out > $1.added.log; rm runlog.out
+time $PYTHON DWC2CSpace.py $1.input.csv ../config/csvimport.cfg ../config/fields.collectors.csv ../config/template.collectors.xml $1.add-audit.csv /dev/null /dev/null add $2 > $1.runlog.out 2>&1
+grep -v Insec $1.runlog.out > $1.added.log; rm $1.runlog.out
 rm $1.inprogress.log
 
--- ucjeps/apps/csvimport/validate-add.sh
+++ ucjeps/apps/csvimport/validate-add.sh
@@ -3,8 +3,8 @@ cd $( dirname "${BASH_SOURCE[0]}" )
 PYTHON=python2.7
 set -x
 touch $1.inprogress.log
-rm -f runlog.out
-nohup time $PYTHON DWC2CSpace.py $1.input.csv ../config/csvimport.cfg ../config/DWC2CSpace-v2.csv ../config/collectionobject-v2.xml $1.valid.csv $1.invalid.csv $1.terms.csv validate $2 > runlog.out 2>&1
+rm -f $1.runlog.out
+time $PYTHON DWC2CSpace.py $1.input.csv ../config/csvimport.cfg ../config/DWC2CSpace-v2.csv ../config/collectionobject-v2.xml $1.valid.csv $1.invalid.csv $1.terms.csv validate $2 > $1.runlog.out 2>&1
 mv $1.valid.csv $1.add.csv
-grep -v Insec runlog.out > $1.validated.log; rm runlog.out
+grep -v Insec $1.runlog.out > $1.validated.log; rm $1.runlog.out
 rm $1.inprogress.log
