51aa67fee730a494cf8465d8864dcfc1b8482bdc
John Lowe <jblowe@berkeley.edu>
2019-04-10 09:57:15 -0700

    CC-444: incremental improvements to microalgae 'helpers'

---
+++ ucjeps/apps/csvimport/helpers/weird_names.pl
@@ -0,0 +1,4 @@
+s/Codium guine..nse/Codium guineense/g;
+s/Lithophyllum samo..nse/Lithophyllum samoense Foslie/g;
+s/Hydrolithon samo..nse/Hydrolithon samoense/g;
+s/Dictyota guine..nsis/Dictyota guineensis/g;
